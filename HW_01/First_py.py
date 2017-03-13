61566fc61ce07393fd4c38d08eb129c38360ffd6
vodachuk <vodachuk@gmail.com>
2017-03-13 21:38:22 +0200

    Add new First_py.py

---
+++ HW_01/First_py.py
@@ -0,0 +1,13 @@
+'''
+Created on Mar 13, 2017
+
+@author: vodachuk
+'''
+str_1 = "aaammmmuuuuuhhhhjjjjkkkkeeeeyyytttddkckopid"
+print str_1
+print "count of o: ",str_1.count("o")
+print "count of a: ",str_1.count("a")
+print "count of i: ",str_1.count("i")
+print "count of j: ",str_1.count("j")
+print "count of e: ",str_1.count("e")
+print "count of u: ",str_1.count("u")
