1ea1bb63a0e9e3f4a0a800701780e7fde42ca28d
Tararas <tarasdidg@gmail.com>
2017-03-13 20:33:17 +0200

    hw 01

---
+++ HW_1/Hrat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#print ("Hello. I am a python program.")
+#x = raw_input('What is your name? ')
+#print ("Hello there, " + x + "!")
+a = input('a = ')
+b = input('b = ')
+#a = 1
+#b = 2
+y = a*2**b-(a+b)**0.5
+print "You answer ", y, "chut"
+
+str1 = "I'm programer!"
+str2 = "I'm student!"
+str3 = "I'm harcoder!"
+str4 = str1[:-1]+","+str2[-9:-1]+","+str3[3:]
+print str4
