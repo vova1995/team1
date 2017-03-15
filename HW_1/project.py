1481d90fe8ec2f90d0f870907bf1fb298f0312c3
pythoncore231 <tarasdidg@gmail.com>
2017-03-15 22:08:42 +0200

    added hometask_2

--- HW_1/project.py
+++ HW_1/project.py
@@ -1,10 +1,10 @@
 str1 = "arytdgdbvannaeeebbbbiiiiiii"
 
-print str1.count('a'), str1.count('e'), str1.count('i')
+print(str1.count('a'), str1.count('e'), str1.count('i'))
 
 
 d = 24544.784245
 num = "%f" % d
 pos = num.find(".")
 
-print int(num[pos-3]) + int(num[pos-2]) + int(num[pos-1]) + int(num[pos+1]) + int(num[pos+2]) + int(num[pos+3])
+print(int(num[pos-3]) + int(num[pos-2]) + int(num[pos-1]) + int(num[pos+1]) + int(num[pos+2]) + int(num[pos+3]))
