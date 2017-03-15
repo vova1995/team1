1faa3023e540e59dec3d65c4f1dd130a27a2fdc5
pythoncore231 <tarasdidg@gmail.com>
2017-03-15 22:13:10 +0200

    chenge hometask_2

--- HW_1/hometask_2.py
+++ HW_1/hometask_2.py
@@ -22,3 +22,15 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
 
 print(string)
+
+
+str1 = "arytdgdbvannaeeebbbbiiiiiii"
+
+print(str1.count('a'), str1.count('e'), str1.count('i'))
+
+
+d = 24544.784245
+num = "%f" % d
+pos = num.find(".")
+
+print(int(num[pos-3]) + int(num[pos-2]) + int(num[pos-1]) + int(num[pos+1]) + int(num[pos+2]) + int(num[pos+3]))
--- HW_1/project.py
+++ HW_1/project.py
@@ -1,10 +1,24 @@
-str1 = "arytdgdbvannaeeebbbbiiiiiii"
+string = """
+The Zen of Python, by Tim Peters
 
-print(str1.count('a'), str1.count('e'), str1.count('i'))
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
 
-
-d = 24544.784245
-num = "%f" % d
-pos = num.find(".")
-
-print(int(num[pos-3]) + int(num[pos-2]) + int(num[pos-1]) + int(num[pos+1]) + int(num[pos+2]) + int(num[pos+3]))
+print(string)
